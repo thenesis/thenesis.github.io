6593c288dd45a37055e2ad0736ce8cb4d7cf0141
GuillermoFerro <thenesis@gmail.com>
2018-03-07 14:23:28 -0500

    Test Garand template

---
+++ assets/script/main.js
@@ -0,0 +1,32 @@
+try{Typekit.load();}catch(e){}
+
+var feed = new Instafeed({
+		get: 'user',
+		userId: 6240580462,
+		accessToken: '4dce4b9a9f2743c1a0155a87e7fcbd71',
+		target: 'instagram',
+		resolution: 'standard_resolution',
+		after: function() {
+			var el = document.getElementById('instagram');
+			if (el.classList)
+				el.classList.add('show');
+			else
+				el.className += ' ' + 'show';
+		}
+});
+
+window.onload = function() {
+	feed.run();
+
+	var _gauges = _gauges || [];
+	(function() {
+		var t   = document.createElement('script');
+		t.type  = 'text/javascript';
+		t.async = true;
+		t.id    = 'gauges-tracker';
+		t.setAttribute('data-site-id', '4d5aa2a95ffe1f587b000001');
+		t.src = '//secure.gaug.es/track.js';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(t, s);
+	})();
+};
